80bbf25df93561a80be1210123c6a533d6be5534
andre <a.bukala@negesi.pl>
2018-01-16 15:05:18 +0100

    Fixed normalization of images

--- features.py
+++ features.py
@@ -76,18 +76,11 @@ def get_sift(X, kmeans=None, k=500, multi=100, n_init=1, max_iter=10):
 
 def get_pix(X):
     # Normalize images
-    X = _normalize_images(X)
+    X = [img/256 for img in X]
     # Flatten data
     X = [x.flatten() for x in X]
 
     return X
-
-
-def _normalize_images(X):
-    v_min = X.min(axis=(0, 1), keepdims=True)
-    v_max = X.max(axis=(0, 1), keepdims=True)
-
-    return (X - v_min)/(v_max - v_min)
 
 
 def normalize_hist(X_train, X_test):

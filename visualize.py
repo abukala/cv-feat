f6a4a4b6f8d4a9f4fcd9906eba684d6d9bbb65eb
andre <a.bukala@negesi.pl>
2018-02-12 12:06:00 +0100

    Fixed plotting

--- visualize.py
+++ visualize.py
@@ -30,19 +30,19 @@ def visualize(feature, noise_type, file_name=None):
                     assert len(selection) == 1
 
                     for training_noise in ['yes', 'no']:
-                        rows.append([dataset, classifier, noise_level, selection.iloc[0]['Score'], training_noise])
+                        rows.append([dataset, classifier, noise_level, eval(selection.iloc[0]['Score']), training_noise])
                 else:
                     for training_noise in ['yes', 'no']:
                         subselection = selection[selection['Train_Noise'] == training_noise]
 
                         assert len(subselection) == 1
 
-                        rows.append([dataset, classifier, noise_level, subselection.iloc[0]['Score'], training_noise])
-
+                        rows.append([dataset, classifier, eval(noise_level), eval(subselection.iloc[0]['Score']), training_noise])
+    noise_levels = [eval(level) for level in noise_levels]
+    if noise_type is not 'lres':
+        noise_levels = [round(level, 2) for level in noise_levels]
     df = pd.DataFrame(rows, columns=['dataset', 'classifier', 'noise level', 'accuracy', 'training noise'])
-
     grid = sns.FacetGrid(df, row='classifier', col='dataset', hue='training noise', legend_out=True)
-    noise_levels = [str(round(float(level), 2)) for level in noise_levels]
     grid.set(ylim=(0.0, 1.0), xticks=range(len(noise_levels)))
     grid.set_xticklabels(noise_levels, rotation=90)
     grid.map(plt.plot, 'accuracy')

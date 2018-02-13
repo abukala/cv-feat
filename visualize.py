3d42843b02c85cb530ae1d9da53902b0c3dae1ae
andre <a.bukala@negesi.pl>
2018-02-13 12:09:36 +0100

    Fixed visualize.py for changed test case. Trials without noise will have specific noise level set now (scaling factor=1 for lower resolution, 0 for others)

--- visualize.py
+++ visualize.py
@@ -16,7 +16,6 @@ def visualize(feature, noise_type, file_name=None):
         'Feature': feature,
         'Noise_Type': noise_type
     }, fetch='all', database_path=databases.FINISHED_PATH))
-    trials.loc[trials['Noise_Level'] == 'none', 'Noise_Level'] = 0
     noise_levels = trials['Noise_Level'].unique()
     rows = []
 
@@ -24,21 +23,14 @@ def visualize(feature, noise_type, file_name=None):
         for classifier in CLASSIFIERS:
             for noise_level in noise_levels:
                 selection = trials[(trials['Dataset'] == dataset) & (trials['Classifier'] == classifier) & (trials['Noise_Level'] == noise_level)]
+                for training_noise in ['yes', 'no']:
+                    subselection = selection[selection['Train_Noise'] == training_noise]
 
-                if noise_level == 0:
-                    assert len(selection) == 1
+                    assert len(subselection) == 1
 
-                    for training_noise in ['yes', 'no']:
-                        rows.append([dataset, classifier, noise_level, eval(selection.iloc[0]['Score']), training_noise])
-                else:
-                    for training_noise in ['yes', 'no']:
-                        subselection = selection[selection['Train_Noise'] == training_noise]
-
-                        assert len(subselection) == 1
-
-                        rows.append([dataset, classifier, eval(noise_level), eval(subselection.iloc[0]['Score']), training_noise])
+                    rows.append([dataset, classifier, eval(noise_level), eval(subselection.iloc[0]['Score']), training_noise])
     noise_levels = [eval(level) for level in noise_levels]
-    if noise_type is not 'lres':
+    if noise_type != 'lres':
         noise_levels = [round(level, 2) for level in noise_levels]
     df = pd.DataFrame(rows, columns=['dataset', 'classifier', 'noise level', 'accuracy', 'training noise'])
     grid = sns.FacetGrid(df, row='classifier', col='dataset', hue='training noise', legend_out=True)

1bee0dda7993c662babc365c6b045c531925d301
andre <a.bukala@negesi.pl>
2018-03-24 21:55:58 +0100

    Fixed FERET array extension

--- datasets/feret.py
+++ datasets/feret.py
@@ -16,18 +16,19 @@ def _load_batch(path, subset=None):
                 continue
         img.append(imread(bz2.BZ2File(file.open(mode='rb'))))
         cls.append(int(file.name[:5]))
-    return np.array(img), np.array(cls)
+    return img, cls
 
 
 def load_data(subset=None, train_ratio=0.6):
     img, cls = _load_batch(FIRST_BATCH, subset)
     img2, cls2 = _load_batch(SECOND_BATCH, subset)
-    img = np.append(img, img2)
-    cls = np.append(cls, cls2)
+    img.extend(img2)
+    cls.extend(cls2)
     assert len(img) == len(cls)
     choices = np.arange(len(img))
     split = int(len(choices)*train_ratio)
     np.random.shuffle(choices)
+    img = np.array(img)
     X_train, y_train = img[choices[:split]], cls[choices[:split]]
     X_test, y_test = img[choices[split:]], cls[choices[split:]]
 

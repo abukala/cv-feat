8e7ff601b2dd078aa6c7429d8baccb137f134509
andre <a.bukala@negesi.pl>
2018-04-06 16:45:09 +0200

    Modified slurm script: - max time 72hrs - added loading fftw module - added optional arguments for script

--- slurm.sh
+++ slurm.sh
@@ -2,8 +2,9 @@
 
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=24:00:00
+#SBATCH --time=72:00:00
 
+module add plgrid/libs/fftw/3.3.7
 module add plgrid/tools/python/3.5.2
 
-python3 ${1}
+python3 ${1} ${@:2}

37c485f96eaf70ab1c20cee5d1bbea65dbdee3d1
andre <a.bukala@negesi.pl>
2018-04-10 17:28:32 +0200

    Fix @ reading hog grid-search results

--- hog_results.py
+++ hog_results.py
@@ -6,23 +6,24 @@ import hog
 if __name__ == '__main__':
     assert len(sys.argv) == 2
     assert sys.argv[1] in ['stl10', 'gtsrb', 'mnist', 'feret']
+
+    check = {}
+    for clf in ['KNN', 'SVM', 'RFC', 'LDA']:
+        for cells_per_block in range(1,4):
+            for pixels_per_cell in hog.hog_params[sys.argv[1]]['pixels_per_cell']:
+                check[(clf, cells_per_block, pixels_per_cell[0])] = False
+
     filename = '%s.json' % sys.argv[1]
     filepath = hog.RESULTS_DIR / filename
 
     with filepath.open(mode='r') as f:
         data = [json.loads(line) for line in f.readlines()]
 
-    check = {}
     top_score = 0
     top_trial = None
 
-    for clf in ['KNN', 'SVM', 'RFC', 'LDA']:
-        for cells_per_block in range(1,4):
-            for pixels_per_cell in hog.hog_params[sys.argv[1]]['pixels_per_cell']:
-                check[(clf, str((cells_per_block, cells_per_block)), str(pixels_per_cell))] = False
-
     for trial in data:
-        check[(trial['clf'], str(trial['cells_per_block']), str(trial['pixels_per_cell']))] = True
+        check[(trial['clf'], trial['cells_per_block'][0], trial['pixels_per_cell'][0])] = True
         if trial['score'] > top_score:
             top_score = trial['score']
             top_trial = trial

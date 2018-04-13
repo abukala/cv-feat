987f7e56424309b2bc489c062deb9310fcb45ceb
andre <a.bukala@negesi.pl>
2018-04-13 12:02:38 +0200

    Added module loaders to slurm script

--- slurm.sh
+++ slurm.sh
@@ -4,7 +4,9 @@
 #SBATCH --ntasks-per-node=10
 #SBATCH --time=72:00:00
 
-module add plgrid/libs/fftw/3.3.7
 module add plgrid/tools/python/3.5.2
+module add plgrid/libs/libpng
+module add plgrid/libs/libjpeg-turbo
+module add plgrid/libs/fftw/3.3.7
 
 python3 ${1} ${@:2}

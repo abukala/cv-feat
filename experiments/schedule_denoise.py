31fe3b880b8de4018bcd992f9f5ea4e4c1a23c03
Andrzej Bukała <a.bukala@negesi.pl>
2018-06-07 14:48:47 +0200

    debug

--- experiments/schedule_denoise.py
+++ experiments/schedule_denoise.py
@@ -36,8 +36,9 @@ for dataset in ['gtsrb', 'stl10', 'feret', 'mnist']:
                     noise_level_range = np.arange(nr['min']+nr['step'], nr['max']+nr['step'], nr['step'])
                     for num in noise_level_range:
                         num = round(num, 3)
-                        print(params[dataset]['denoise'][noise_type])
-                        print(num)
+                        if num not in params[dataset]['denoise'][noise_type]:
+                            print(params[dataset]['denoise'][noise_type])
+                            print(num, noise_type, dataset)
                         trial = {
                             'Dataset': dataset,
                             'Feature': feature,
